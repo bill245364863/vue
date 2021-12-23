bca8c641019069db78516955e5ffa088ed7aaa7c
huangxiaotao <245364863@qq.com>
2021-12-23 21:44:23 +0800

    init

---
+++ src/components/Hello.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{msg}} </h1>
+</template>
+
+<script>
+export default {
+  name: "Hello",
+  data(){
+    return{
+      msg:"大家好啊"
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
